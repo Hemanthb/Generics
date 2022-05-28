2d17183a8296f87f66f7b08c009ab533140da8b4
hem_b <hemanthb93@gmail.com>
2022-05-28 14:38:42 +0530

    [Hemanth]:Prints an int,double and char array

---
+++ PrintArray/PrintArray/PrintArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArray
+{
+    class PrintArray
+    {
+        public void PrintIntArray(int[] intArray)
+        {
+            foreach (var item in intArray)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        public void PrintDoubleArray(double[] doubleArray)
+        {
+            foreach (var item in doubleArray)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        public void PrintCharArray(char[] charArray)
+        {
+            foreach (var item in charArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ PrintArray/PrintArray/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] intArray = new int[5] { 1, 2, 3, 4, 5 };
+double[] doubleArray = new double[6] { 1.0, 2.0, 4.50, 34.56, 590.25, 6.1 };
+char[] chars = new char[3] { 'h', 'e', 'm' };
+
+PrintArray.PrintArray print = new PrintArray.PrintArray();
+Console.WriteLine("-------Int Array----------------");
+print.PrintIntArray(intArray);
+Console.WriteLine("-------DoubleArray--------------");
+print.PrintDoubleArray(doubleArray);
+Console.WriteLine("-------Char Array---------------");
+print.PrintCharArray(chars);
